0dcd883b44fefa7eb4ae07caa7401cde49777830
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-10-26 20:28:29 +0530

    def gcd(a,b):     if(b==0):         return abs(a)     else:         return gcd(b,a%b) for _ in range(int(input())):     n,a,b,k=map(int,input().split())     c=gcd(a,b)     lcm=(a*b)//c     x=n//a+n//b-2*n//lcm     if x>=k:         print('Win')     else:         print('Lose')

--- Appy_and_Contest.py
+++ Appy_and_Contest.py
@@ -1,14 +1,14 @@
-import math
-t=int(input())
-for k in range(t):
+def gcd(a,b):
+    if(b==0):
+        return abs(a)
+    else:
+        return gcd(b,a%b)
+for _ in range(int(input())):
     n,a,b,k=map(int,input().split())
-    p=n//a
-    q=n//b
-    c=math.gcd(a,b)
-    d=(a*b)//c
-    s=n//d
-    if((p+q-2*s)>=k):
+    c=gcd(a,b)
+    lcm=(a*b)//c
+    x=n//a+n//b-2*n//lcm
+    if x>=k:
         print('Win')
     else:
         print('Lose')
-    
